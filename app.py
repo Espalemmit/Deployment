3acc8a1d4fce527e994371d7b1f3dfdf734c165d
Tu Huynh <tuhla.bpc@vnpt.vn>
2024-01-10 17:24:53 +0700

    API Add

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 cv = pickle.load(open("models/cv.pkl", "rb"))
@@ -15,11 +15,21 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    email = request.form.get('content')
+    data = request.form.get('content')
+    email = data['content']
     tokenized_email = cv.transform([email])
     prediction = clf.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html", prediction=prediction, email=email)
+
+@app.route("/api/predict", methods=["POST"])
+def api_predict():
+    data = request.get_json(force=True)
+    email = data['content']
+    tokenized_email = cv.transform(email)
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({'prediction' : prediction, 'email' : email})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
